9f947cc8d8315045498654431bc8f67bc3fb4d8b
aseembler <adamredmi66@gmail.com>
2020-11-05 02:41:34 +0100

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+let theNumber = Number(prompt("daj numer"));
+if (!Number.isNaN(theNumber)) {
+console.log("twoj numer to pierwiastek kwadratowy z " + theNumber * theNumber);
+} else {
+ console.log("Potrzebny numer");
+
+}
+
+
+
+/*
+function 
+brb("Osoba","Jaka")
+ {
+     return "Osoba" + "Jaka";
+
+
+ }
+ var Osoba = "Basia", Jaka = "jest madra.", Jaka2 = "i fajna";
+ */
